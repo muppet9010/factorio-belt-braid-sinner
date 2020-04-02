974a912193c78b3e6e1431d4c1a4d8ad69604d14
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-04-02 16:54:22 +0000

    update Utils

--- utility/events.lua
+++ utility/events.lua
@@ -1,48 +1,46 @@
-local Utils = require("utility/utils")
 --local Logging = require("utility/logging")
 
 local Events = {}
 MOD = MOD or {}
 MOD.events = MOD.events or {}
 MOD.customEventNameToId = MOD.customEventNameToId or {}
+MOD.eventFilters = MOD.eventFilters or {}
 
 -- Called either from the root of Control.lua or from OnLoad for vanilla events and custom events.
 -- Filtered events have to expect to recieve results outside of their filter. As an event can only be registered one time, with multiple instances the most lienient or merged filters for all instances must be applied.
-Events.RegisterEvent = function(eventName, filterData)
+-- Returns the eventId, useful for  custom event names when you need to store the eventId to return via a remote interface call.
+Events.RegisterEvent = function(eventName, thisFilterName, thisFilterData)
     if eventName == nil then
         error("Events.RegisterEvent called with missing arguments")
     end
-    local eventId
-    if Utils.GetTableKeyWithValue(defines.events, eventName) ~= nil then
+    local eventId, filterData
+    if type(eventName) == "number" then
         eventId = eventName
-        if filterData ~= nil then
+        if thisFilterData ~= nil then
+            MOD.eventFilters[thisFilterName] = thisFilterData
             local currentFilter, currentHandler = script.get_event_filter(eventId), script.get_event_handler(eventId)
-            if currentHandler ~= nil then
-                if currentFilter == nil then
-                    --event registered with no filter, so already fully lienent.
-                    return
-                else
-                    --add new filter to old filter and let it be re-applied.
-                    currentFilter[1].mode = "or"
-                    for _, currentFilterEntry in pairs(currentFilter) do
-                        table.insert(filterData, currentFilterEntry)
+            if currentHandler ~= nil and currentFilter == nil then
+                --an event is registered already and has no filter, so already fully lienent.
+                return
+            else
+                --add new filter to any existing old filter and let it be re-applied.
+                filterData = {}
+                for _, filterTable in pairs(MOD.eventFilters) do
+                    filterTable[1].mode = "or"
+                    for _, filterEntry in pairs(filterTable) do
+                        table.insert(filterData, filterEntry)
                     end
                 end
-            --else
-            --no current handler so just let our filter be set.
             end
-        --else
-        --we're not filtering so just blindly overwrite whatever is there safely.
         end
     elseif MOD.customEventNameToId[eventName] ~= nil then
         eventId = MOD.customEventNameToId[eventName]
-    elseif type(eventName) == "number" then
-        eventId = eventName
     else
         eventId = script.generate_event_name()
         MOD.customEventNameToId[eventName] = eventId
     end
     script.on_event(eventId, Events._HandleEvent, filterData)
+    return eventId
 end
 
 --Called from the root of Control.lua for custom inputs (key bindings) as their names are handled specially.
@@ -97,13 +95,11 @@ end
 Events.RaiseEvent = function(eventData)
     eventData.tick = game.tick
     local eventName = eventData.name
-    if defines.events[eventName] ~= nil then
-        script.raise_event(defines.events[eventName], eventData)
+    if type(eventName) == "number" then
+        script.raise_event(eventName, eventData)
     elseif MOD.customEventNameToId[eventName] ~= nil then
         local eventId = MOD.customEventNameToId[eventName]
         script.raise_event(eventId, eventData)
-    elseif type(eventName) == "number" then
-        script.raise_event(eventName, eventData)
     else
         error("WARNING: raise event called that doesn't exist: " .. eventName)
     end
@@ -113,12 +109,10 @@ end
 Events.RaiseInternalEvent = function(eventData)
     eventData.tick = game.tick
     local eventName = eventData.name
-    if defines.events[eventName] ~= nil then
+    if type(eventName) == "number" then
         Events._HandleEvent(eventData)
     elseif MOD.customEventNameToId[eventName] ~= nil then
         eventData.name = MOD.customEventNameToId[eventName]
-        Events._HandleEvent(eventData)
-    elseif type(eventName) == "number" then
         Events._HandleEvent(eventData)
     else
         error("WARNING: raise event called that doesn't exist: " .. eventName)
